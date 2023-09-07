9779018d6dd20d5bdb8fbd41d60db62a8e26b357
cc <cc@ccc.local>
2023-09-07 17:15:22 +0800

    feat: RenderObjectWidget

--- lib/render_hexagonal_button.dart
+++ lib/render_hexagonal_button.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/rendering.dart';
 
 class RenderHexagonalButton extends RenderShiftedBox {
@@ -45,53 +43,5 @@ class RenderHexagonalButton extends RenderShiftedBox {
     if (_onTap == onTap) return;
     _onTap = onTap;
     markNeedsSemanticsUpdate();
-  }
-
-  @override
-  void performLayout() {
-    size = _computeSize(
-        constraints: constraints,
-        layoutChild: (RenderBox child, BoxConstraints constraint) {
-          child.layout(constraints, parentUsesSize: true);
-          return child.size;
-        });
-    if (child != null) {
-      /// parent决定position
-      final childParentData = child!.parentData! as BoxParentData;
-
-      /// 对齐方式
-      childParentData.offset = Alignment.center.alongOffset(size - child!.size as Offset);
-    }
-  }
-
-  @override
-  Size computeDryLayout(BoxConstraints constraints) {
-    return _computeSize(
-      constraints: constraints,
-      layoutChild: (RenderBox child, BoxConstraints constraint) => child.getDryLayout(constraints),
-    );
-  }
-
-  Size _computeSize({required BoxConstraints constraints, required ChildLayouter layoutChild}) {
-    if (child != null) {
-      /// 向下传递约束，向上传递尺寸
-
-      /// 根据padding等确定约束
-      final childConstraints = constraints.deflate(padding);
-
-      /// 传递约束，确定 child 的尺寸
-      final childSize = layoutChild(child!, childConstraints);
-      final clipsWidth = clipSize * 2;
-
-      /// 根据 padding、clipSize 确定具体尺寸
-      final targetSize = Size(
-        clipsWidth + childSize.width + padding.horizontal,
-        max(40, childSize.height + padding.vertical),
-      );
-
-      /// 更精确的尺寸
-      return constraints.constrain(targetSize);
-    }
-    return Size(clipSize * 2, 40);
   }
 }
